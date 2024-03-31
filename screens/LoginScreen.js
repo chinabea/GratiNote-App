7fb2b6dc550d78b3e33875959506d56edc9e8a7b
China T. Bea <chibea@my.cspc.edu.ph>
2024-03-31 16:56:09 +0800

    design frontend

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,19 +5,24 @@ import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
 
+
 const themeColor = {
-  bg: '#B8B6FF', 
+  // button: '#6C63FF', 
+  lightPurple: '#A6A2FF', 
+  // button: '#AEABFF', 
+  bg: '#EEF1F6', 
+  bgColor: '#B8B6FF', 
 };
 
 export default function LoginScreen () {
   const navigation = useNavigation();
   return (
-    <View className="flex-1 bg-white" style={{backgroundColor: themeColor.bg}}>
+    <View className="flex-1 bg-white" style={{backgroundColor: themeColor.bgColor}}>
       <SafeAreaView className="flex">
         <View className="flex-row justify-start">
           <TouchableOpacity 
             onPress={() => navigation.goBack()}
-            className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
+            className="bg-white p-2 rounded-2xl ml-4">
             <ArrowLeftIcon size="20" color="black" />
           </TouchableOpacity> 
         </View>
@@ -41,21 +46,21 @@ export default function LoginScreen () {
           <TouchableOpacity className="flex items-end mb-5">
             <Text className="text-gray-700">Forgot Password?</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="py-3 bg-yellow-400 rounded-xl">
-            <Text className="font-xl font-bold text-center text-gray-700">Login</Text>
+          <TouchableOpacity className="py-3 rounded-3xl" style={{backgroundColor: themeColor.lightPurple }}>
+            <Text className="font-xl font-bold text-center text-white">LOGIN</Text>
           </TouchableOpacity>
         </View>
-        <Text className="text-xl text-gray-700 font-bold text-center py-5">
+        <Text className="text-sm text-gray-700 font-bold text-center py-5">
           Or
         </Text>
         <View className="flex-row justify-center space-x-12">
           <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
             <Image source={require('../assets/icons8-google-30.png')} 
-            className="w-8 h-8"/>
+            className="w-7 h-7"/>
           </TouchableOpacity>
           <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
             <Image source={require('../assets/icons8-facebook-48.png')} 
-            className="w-8 h-8"/>
+            className="w-7 h-7"/>
           </TouchableOpacity>
           {/* <TouchableOpacity className="p-2 bg-gray-100 rounded-2xl">
             <Image source={require('../assets/icons8-facebook-48.png')} 
@@ -65,7 +70,7 @@ export default function LoginScreen () {
         <View className="flex-row justify-center mt-7">
           <Text className="text-gray-500 font-semibold">Don't have an account?</Text>
           <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-            <Text className="font-semibold text-yellow-500"> Sign Up</Text>
+            <Text className="font-semibold" style={{ color: themeColor.lightPurple}}> Sign Up</Text>
           </TouchableOpacity>
         </View>
 
